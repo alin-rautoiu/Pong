f1d9618006b2efbeb550d4fb946304bf8ab3e166
alin-rautoiu <alin.rautoiu@cti.pub.ro>
2014-01-15 17:11:55 +0200

    Introdus posibilitatea adaugarii de celule din aplicate
    Dar pierdut schimbarile estetice

--- Pong/Form1.Designer.cs
+++ Pong/Form1.Designer.cs
@@ -28,21 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(716, 279);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(330, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(740, 334);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
 
     }
 }
--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -14,11 +14,18 @@ namespace Pong
         double x;
         double y;
         double speed = 1;
+        bool Paused;
+        Timer tick;
         public List<Cell> cells = new List<Cell>();
         public Form1()
         {
-            
             InitializeComponent();
+            Paused = true;
+            panel1.Paint += Form1_Paint;
+            button1.Click += button1_Click;
+
+            //MouseClick += Form1_MouseClick;
+            panel1.Click += panel1_Click;
 
             //Toad
             /*cells.Add(new Cell(40,80));
@@ -36,35 +43,71 @@ namespace Pong
             cells.Add(new Cell(240, 120));
             cells.Add(new Cell(200, 80));
 
-            Timer tick = new Timer();
+            tick = new Timer();
             tick.Tick += new EventHandler(logic);
             tick.Interval = 250;
-            tick.Start();
-            tick.Enabled = true;            
+            //tick.Enabled = true;            
+            tick.Stop();
+        }
+
+        void panel1_Click(object sender, EventArgs e)
+        {
+            AddCells();
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            switch (Paused)
+            {
+                case false:
+                    tick.Stop();
+                    button1.Text = "Start";
+                    Paused = true;
+                    break;
+
+                case true:
+                    tick.Start();
+                    button1.Text = "Pause";
+                    Paused = false;
+                    break;
+            }
+        }
+
+        public void AddCells()
+        {
+            if (Paused)
+            {
+                Cell newCell = new Cell(MousePosition.X - (MousePosition.X - panel1.Left - this.Left - 10) % 40 - panel1.Left - this.Left - 10, MousePosition.Y - (MousePosition.Y - panel1.Top - this.Top - 30) % 40 - panel1.Top - this.Top - 30);
+
+                if (cells.Contains(newCell))
+                {
+                    cells.Remove(newCell);
+                }
+                else
+                {
+                    cells.Add(newCell);
+                }
+                panel1.Refresh();
+            }
         }
 
         public void logic(object sender, EventArgs e)
         {
             cells = Board.Iterate(cells);
-            this.Refresh();
+            panel1.Refresh();
         }
-        /*public void logic(object sender, EventArgs e)
-        {
-            x += speed;
-            this.Refresh();
-        }*/
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             x += speed;
-            System.Drawing.Graphics graphics;
-            var graphicsObj = this.CreateGraphics();
+            //var graphicsObj = this.CreateGraphics();
+            var g = e.Graphics;
             Pen myPen = new Pen(System.Drawing.Color.Red, 4);
             foreach (var cell in cells)
             {
                 if (true)
                 {
-                    graphicsObj.DrawRectangle(myPen, new Rectangle(cell.X / 5, cell.Y / 5, cell.Size / 10, cell.Size / 10));
+                    g.DrawRectangle(myPen, new Rectangle(cell.X, cell.Y, cell.Size, cell.Size));
                     cell.Drew = true;
                 }
             }
